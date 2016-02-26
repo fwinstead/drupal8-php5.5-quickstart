255143b13368a64bc35651be7f2fe8f3a32ebd97
Frank Winstead <frank.winstead@gmail.com>
2016-02-25 21:34:23 -0500

    Update:

--- misc/make.sh
+++ misc/make.sh
@@ -120,37 +120,10 @@ fi
 # pass NODRUPAL=1 to skip step
 if test -z "${NODDRUPAL}"
 then
-	pushd ${OPENSHIFT_REPO_DIR}
-		VERSION="8.0.3"
-		DRUPAL="drupal-${VERSION}" # NEED grab latest version
-		FNAME="${DRUPAL}.tar.gz"
-		SETTINGS="settings.php"
-		cd ${OPENSHIFT_REPO_DIR}
-		# Get Drupal 8.x 
-		if test \! -f "${FNAME}"; then wget "https://www.drupal.org/files/projects/${FNAME}" fi
-		time tar -zxf "${FNAME}" > /dev/null
-
-		cd ${OPENSHIFT_REPO_DIR}/${DRUPAL}/sites/default
-		cp "default.${SETTINGS}" "${SETTINGS}"
-		chmod a+w . "${SETTINGS}"
-
-		# Drupal settings.php
-		cat "${OPENSHIFT_REPO_DIR}/misc/openshift.drupal.append.settings.php" >> "${SETTINGS}"
-
-		# "Trusted Host Settings" Security fix
-		# could be more secure by limiting rhcloud.com
-		# https://www.drupal.org/node/1992030
-		# https://www.drupal.org/node/2410395
-
-		echo '# Trusted Host Settings security fix' >> "${SETTINGS}"
-		echo '$settings["trusted_host_patterns"] = array(' >> "${SETTINGS}"
-		# loop through REDIRECTS
-
-		for i in ${REDIRECTS[*]}
-		do
-			echo "'${i}'"  >> "${SETTINGS}"
-		done
-		echo ');' >> "${SETTINGS}"
-	popd
+	bash ${OPENSHIFT_REPO_DIR}/misc/drupal.install.sh
 fi
+
+echo "Normal Finish."
+return 0
+# ## ### ####
 
